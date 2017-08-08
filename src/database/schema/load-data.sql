9e5f39e2d2f48c0f9fef920f1623d813dda3a039
yaseenagag <yaseen.uc@gmail.com>
2017-08-07 18:01:12 -0700

    setup schema and begin seed data

---
+++ src/database/schema/load-data.sql
@@ -0,0 +1,45 @@
+-- Find out how to store credit card number better
+INSERT INTO customer (name, username, address, phone_number, payment) VALUES ('Tamiko Lorant', 'tlorant0', '22756 Morningstar Lane', '86-(577)779-6289', 'check');
+INSERT INTO customer (name, username, address, phone_number, payment) VALUES ('Timothee Storm', 'tstorm1', '356 Dayton Court', '55-(530)930-3345', 'credit - 0000-0000-0000-0000');
+INSERT INTO customer (name, username, address, phone_number, payment) VALUES ('Tawsha Orans', 'torans2', '44692 Starling Place', '62-(528)665-8550', 'check');
+INSERT INTO customer (name, username, address, phone_number, payment) VALUES ('Cacilie Begley', 'cbegley3', '2 Portage Alley', '62-(990)218-7676', 'credit - 0000-0000-0000-0000');
+INSERT INTO customer (name, username, address, phone_number, payment) VALUES ('Alberik Kittoe', 'akittoe4', '8 Delladonna Center', '86-(671)581-9454', 'cash');
+
+-- Pizza
+INSERT INTO pizza (size, crust, price, discount_price) VALUES ('small', 'thin', null, null);
+INSERT INTO pizza (size, crust, price, discount_price) VALUES ('small', 'thick', null, null);
+INSERT INTO pizza (size, crust, price, discount_price) VALUES ('medium', 'thin', null, null);
+INSERT INTO pizza (size, crust, price, discount_price) VALUES ('small', 'thick', null, null);
+INSERT INTO pizza (size, crust, price, discount_price) VALUES ('extra-large', 'thin', null, null);
+INSERT INTO pizza (size, crust, price, discount_price) VALUES ('extra-large', 'thin', null, null);
+INSERT INTO pizza (size, crust, price, discount_price) VALUES ('small', 'thick', null, null);
+INSERT INTO pizza (size, crust, price, discount_price) VALUES ('large', 'thick', null, null);
+INSERT INTO pizza (size, crust, price, discount_price) VALUES ('small', 'thin', null, null);
+INSERT INTO pizza (size, crust, price, discount_price) VALUES ('medium', 'thin', null, null);
+INSERT INTO pizza (size, crust, price, discount_price) VALUES ('small', 'thick', null, null);
+INSERT INTO pizza (size, crust, price, discount_price) VALUES ('large', 'thick', null, null);
+INSERT INTO pizza (size, crust, price, discount_price) VALUES ('small', 'thin', null, null);
+INSERT INTO pizza (size, crust, price, discount_price) VALUES ('extra-large', 'thin', null, null);
+INSERT INTO pizza (size, crust, price, discount_price) VALUES ('extra-large', 'thick', null, null);
+
+-- Drinks
+INSERT INTO drink (description, manufacturer, supplier, price) VALUES ('Coke', 'Coca-Cola', 'Unilever', 1.72);
+INSERT INTO drink (description, manufacturer, supplier, price) VALUES ('Pepsi', 'Pepsi', 'Johnson & Johnson', 3.37);
+INSERT INTO drink (description, manufacturer, supplier, price) VALUES ('Sprite', 'Coca-Cola', 'Unilever', 2.25);
+INSERT INTO drink (description, manufacturer, supplier, price) VALUES ('Fanta', 'Coca-Cola', 'Unilever', 1.95);
+INSERT INTO drink (description, manufacturer, supplier, price) VALUES ('Dr.Pepper', 'Dr.Pepper Snapple Group', 'Johnson & Johnson', 2.67);
+
+-- Ingredients
+
+INSERT INTO ingerdient(name) VALUES ("pepperoni");
+INSERT INTO ingerdient(name) VALUES ("sausage");
+INSERT INTO ingerdient(name) VALUES ("mushrooms");
+INSERT INTO ingerdient(name) VALUES ("onions");
+INSERT INTO ingerdient(name) VALUES ("pineapple");
+INSERT INTO ingerdient(name) VALUES ("olives");
+INSERT INTO ingerdient(name) VALUES ("chicken");
+
+-- Preferences
+
+
+-- Toppings
---
+++ src/database/schema/schema.sql
@@ -0,0 +1,46 @@
+DROP TABLE IF EXISTS customer
+CREATE TABLE customer(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  username VARCHAR(50) NOT NULL,
+  address VARCHAR(100) NOT NULL,
+  phone_number INTEGER NOT NULL,
+  payment VARCHAR(50)
+)
+
+DROP TABLE IF EXISTS pizza
+CREATE TABLE pizza(
+  id SERIAL PRIMARY KEY,
+  size VARCHAR(50) NOT NULL,
+  crust VARCHAR(50) NOT NULL,
+  price MONEY NOT NULL,
+  discount_price MONEY NOT NULL
+)
+
+DROP TABLE IF EXISTS drink
+CREATE TABLE drink(
+  id SERIAL PRIMARY KEY,
+  description VARCHAR(50) NOT NULL,
+  manufacturer VARCHAR(50) NOT NULL,
+  supplier VARCHAR(50) NOT NULL,
+  price MONEY NOT NULL
+)
+
+DROP TABLE IF EXISTS ingredient
+CREATE TABLE ingredient(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  price MONEY NOT NULL
+)
+
+DROP TABLE IF EXISTS preference
+CREATE TABLE preference(
+  customer_id INTEGER NOT NULL,
+  ingredient_id INTEGER NOT NULL
+)
+
+DROP TABLE IF EXISTS topping
+CREATE TABLE topping(
+  pizza_id INTEGER NOT NULL,
+  ingredient_id INTEGER NOT NULL
+)
